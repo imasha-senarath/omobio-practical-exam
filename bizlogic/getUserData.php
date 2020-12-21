004917200ed83d36af9f40ce18ff556842e091c0
Imasha Senarath <imashadsenarath@gmail.com>
2020-12-21 12:50:20 +0530

    fivth commit

--- bizlogic/getUserData.php
+++ bizlogic/getUserData.php
@@ -11,7 +11,10 @@
     if($query) {
         $i=0;
         while($userRow = mysqli_fetch_assoc($query)){
+            $response[$i]['id'] = $userRow['id'];
             $response[$i]['name'] = $userRow['name'];
+            $response[$i]['username'] = $userRow['username'];
+            $response[$i]['email'] = $userRow['email'];
             $i++;
         }
     
