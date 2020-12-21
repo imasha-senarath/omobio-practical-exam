2075635ebb2cdc9a3ea05a53965640db58b30f28
Imasha Senarath <imashadsenarath@gmail.com>
2020-12-21 12:32:58 +0530

    third commit

--- bizlogic/getUsers.php
+++ bizlogic/getUsers.php
@@ -1,16 +1,18 @@
 <?php
     $con = mysqli_connect('localhost','root','','Exam');
 
-    $selectUsers = "select * from users";
+    $response = array();
+    $selectUsers = 'select * from user';
     $query = mysqli_query($con, $selectUsers);
 
-    $response = array();
-    $i=0;
-    while($userRow = mysqli_fetch_assoc($query)){
-        $response[$i]['name'] = $userRow['name'];
-        $i++;
+    if($query) {
+        $i=0;
+        while($userRow = mysqli_fetch_assoc($query)){
+            $response[$i]['name'] = $userRow['name'];
+            $i++;
+        }
+    
+        echo json_encode($response, JSON_PRETTY_PRINT);
     }
-
-    echo json_encode($response, JSON_PRETTY_PRINT);
       
 ?>
